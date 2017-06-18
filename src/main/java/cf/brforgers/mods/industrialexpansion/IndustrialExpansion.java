4e48297a6938c1ca6a469e8a650a7d81e100f952
allanf181 <allanfarmelin2001@yahoo.com.br>
2017-06-18 11:39:24 -0300

    Updated libs

--- src/main/java/cf/brforgers/mods/industrialexpansion/IndustrialExpansion.java
+++ src/main/java/cf/brforgers/mods/industrialexpansion/IndustrialExpansion.java
@@ -22,12 +22,12 @@ import org.apache.logging.log4j.Logger;
         name = IndustrialExpansion.MOD_NAME,
         useMetadata = true,
         version = IndustrialExpansion.VERSION,
-        dependencies = "required-after:thermalexpansion@[5.2.0,);"
+        dependencies = "required-after:thermalexpansion@[5.2.2,);"
 )
 public class IndustrialExpansion {
     public static final String MOD_ID = "industrialexpansion";
     public static final String MOD_NAME = "IndustrialExpansion";
-    public static final String VERSION = "1.2.1";
+    public static final String VERSION = "1.2.2";
     //Proxy Identity
     public static final String COMMONPROXY = "cf.brforgers.mods.industrialexpansion.proxy.CommonProxy";
     public static final String CLIENT = "cf.brforgers.mods.industrialexpansion.proxy.ClientProxy";
