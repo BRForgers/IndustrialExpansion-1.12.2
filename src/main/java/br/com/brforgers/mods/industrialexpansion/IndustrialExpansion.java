bcbdb1d0f2fad2d35cca1b63f973844d9a9bee33
Allan Fernando <allanfarmelin2001@yahoo.com.br>
2017-12-08 13:38:59 -0200

    Create zh_CN.lang

--- src/main/java/br/com/brforgers/mods/industrialexpansion/IndustrialExpansion.java
+++ src/main/java/br/com/brforgers/mods/industrialexpansion/IndustrialExpansion.java
@@ -31,7 +31,7 @@ import org.apache.logging.log4j.Logger;
 public class IndustrialExpansion {
     public static final String MOD_ID = "industrialexpansion";
     public static final String MOD_NAME = "IndustrialExpansion";
-    public static final String VERSION = "1.2.3";
+    public static final String VERSION = "1.2.4";
     //Proxy Identity
     public static final String COMMONPROXY = "br.com.brforgers.mods.industrialexpansion.proxy.CommonProxy";
     public static final String CLIENT = "br.com.brforgers.mods.industrialexpansion.proxy.ClientProxy";
